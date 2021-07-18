884288aa2e839d92093df51c86d1ec45b8a4f537
Ranjit Kairi <ranjitkoiri009@gmail.com>
2021-07-18 14:00:19 +0530

    hero-2

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 import "./tailwind.css";
+
+const nav_toggles = document.querySelector(".nav-toggle");
+const navigation = document.getElementById("navigation");
+
+nav_toggles.addEventListener("click", () => {
+  navigation.classList.toggle("hidden");
+});
